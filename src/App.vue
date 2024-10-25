ef87143656f1703423fcec0fb0648553c97d43e8
sirrazmo <sirrazmo@orange.fr>
2024-10-25 16:57:12 +0200

    Première interface de test

--- src/App.vue
+++ src/App.vue
@@ -11,6 +11,40 @@
       </div>
     </section>
   </div>
+
+  <div class="container">
+    <section class="hero is-info">
+      <br>
+      <div class="field">
+        <p class="control has-icons-left">
+          <input class="input" type="text" placeholder="Matricule">
+          <span class="icon is-small is-left">
+            <i class="fa-solid fa-id-card-clip"></i>
+          </span>
+        </p>
+      </div>
+
+      <div class="field">
+        <p class="control has-icons-left">
+          <input class="input" type="password" placeholder="Password">
+          <span class="icon is-small is-left">
+            <i class="fas fa-lock"></i>
+          </span>
+        </p>
+      </div>
+      <div class="field">
+        <div class="columns is-centered">
+          <div class="column is-narrow">
+            <p class="control">
+              <button class="button is-warning is-rounded is-center">
+                Se connecter
+              </button>
+            </p>
+          </div>
+        </div>
+      </div>
+    </section>
+  </div>
 </template>
 
 <script>
