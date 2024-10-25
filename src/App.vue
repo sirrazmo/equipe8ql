6d12f827371c921a7f669779a0f153d97b2b1f11
sirrazmo <sirrazmo@orange.fr>
2024-10-25 17:25:06 +0200

    Réorganisation des pages

--- src/App.vue
+++ src/App.vue
@@ -1,54 +1,19 @@
 <template>
-  <div class="container">
-    <section class="hero is-primary">
-      <div class="hero-body">
-        <p class="title">
-          Bienvenue sur votre appli
-        </p>
-        <p class="subtitle">
-          Votre application de réservation
-        </p>
-      </div>
-    </section>
-  </div>
-
-  <div class="container">
-    <section class="hero is-info">
-      <br>
-      <div class="field">
-        <p class="control has-icons-left">
-          <input class="input" type="text" placeholder="Matricule">
-          <span class="icon is-small is-left">
-            <i class="fa-solid fa-id-card-clip"></i>
-          </span>
-        </p>
-      </div>
-
-      <div class="field">
-        <p class="control has-icons-left">
-          <input class="input" type="password" placeholder="Password">
-          <span class="icon is-small is-left">
-            <i class="fas fa-lock"></i>
-          </span>
-        </p>
-      </div>
-      <div class="field">
-        <div class="columns is-centered">
-          <div class="column is-narrow">
-            <p class="control">
-              <button class="button is-warning is-rounded is-center">
-                Se connecter
-              </button>
-            </p>
-          </div>
-        </div>
-      </div>
-    </section>
+  <div id="app">
+    <h1> Page principale</h1>
+    <Connexion />
   </div>
 </template>
 
 <script>
+import Connexion from './components/Connexion.vue';
+
 export default {
   name: 'App',
-}
+  components: {
+    Connexion,
+  },
+};
 </script>
+
+<style></style>
---
+++ src/components/Connexion.vue
@@ -0,0 +1,63 @@
+<template>
+    <div class="container">
+        <section class="hero is-primary">
+      <div class="hero-body">
+        <p class="title">
+          Bienvenue sur votre appli
+        </p>
+        <p class="subtitle">
+          Votre application de réservation
+        </p>
+      </div>
+    </section>
+  </div>
+
+  <div class="container">
+    <section class="hero is-info">
+      <br>
+      <div class="field">
+        <p class="control has-icons-left">
+          <input class="input" type="text" placeholder="Matricule">
+          <span class="icon is-small is-left">
+            <i class="fa-solid fa-id-card-clip"></i>
+          </span>
+        </p>
+      </div>
+
+      <div class="field">
+        <p class="control has-icons-left">
+          <input class="input" type="password" placeholder="Password">
+          <span class="icon is-small is-left">
+            <i class="fas fa-lock"></i>
+          </span>
+        </p>
+      </div>
+      <div class="field">
+        <div class="columns is-centered">
+          <div class="column is-narrow">
+            <p class="control">
+              <button class="button is-warning is-rounded is-center">
+                Se connecter
+              </button>
+            </p>
+          </div>
+        </div>
+      </div>
+    </section>
+    </div>
+</template>
+
+<script>
+
+export default {
+/* eslint-disable */
+  name: 'connexion',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
