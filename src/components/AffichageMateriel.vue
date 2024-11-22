d9bb9a9107e03703c90db1fa8b1b4289300c0952
sirrazmo <sirrazmo@orange.fr>
2024-11-22 12:21:00 +0100

    Premiere version AffichageMateriel.vue

---
+++ src/components/AffichageMateriel.vue
@@ -0,0 +1,57 @@
+<template>
+
+    <table class="table">
+        <thead>
+            <tr>
+                <th>Nom</th>
+                <th>Telephone</th>
+                <th>Colonne3</th>
+                <th>Colonne4</th>
+                <th>Colonne5</th>
+            </tr>
+        </thead>
+        <tfoot>
+            <tr>
+                <th>Nom</th>
+                <th>Telephone</th>
+                <th>Colonne3</th>
+                <th>Colonne4</th>
+                <th>Colonne5</th>
+            </tr>
+        </tfoot>
+        <tbody>
+            <tr>
+                <td>materiel1</td>
+                <td>0123456789</td>
+                <td>col3</td>
+                <td>col4</td>
+                <td>col5</td>
+            </tr>
+            <tr>
+                <td>materiel2</td>
+                <td>9876543210</td>
+                <td>col3</td>
+                <td>col4</td>
+                <td>col5</td>
+            </tr>
+            <tr>
+                <td>materiel3</td>
+                <td>1111111111</td>
+                <td>col3</td>
+                <td>col4</td>
+                <td>col5</td>
+            </tr>
+            <tr>
+                <td>materiel4</td>
+                <td>2222222222</td>
+                <td>col3</td>
+                <td>col4</td>
+                <td>col5</td>
+            </tr>
+        </tbody>
+    </table>
+
+
+
+
+</template>
