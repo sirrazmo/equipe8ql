87e5e23f7663f148e0d0ca476d99c2fe6d89a5b3
sirrazmo <sirrazmo@orange.fr>
2024-11-22 12:24:48 +0100

    Premiere version AffichageUtilisateur.vue

---
+++ src/components/AffichageUtilisateur.vue
@@ -0,0 +1,57 @@
+<template>
+
+    <table class="table">
+        <thead>
+            <tr>
+                <th>Nom</th>
+                <th>Telephone</th>
+                <th>Colonne3</th>
+                <th>Colonne4</th>
+                <th>Colonne5</th>
+            </tr>
+        </thead>
+        <tfoot>
+            <tr>
+                <th>Nom</th>
+                <th>Telephone</th>
+                <th>Colonne3</th>
+                <th>Colonne4</th>
+                <th>Colonne5</th>
+            </tr>
+        </tfoot>
+        <tbody>
+            <tr>
+                <td>Utilisateur1</td>
+                <td>0123456789</td>
+                <td>col3</td>
+                <td>col4</td>
+                <td>col5</td>
+            </tr>
+            <tr>
+                <td>Utilisateur2</td>
+                <td>9876543210</td>
+                <td>col3</td>
+                <td>col4</td>
+                <td>col5</td>
+            </tr>
+            <tr>
+                <td>Utilisateur3</td>
+                <td>1111111111</td>
+                <td>col3</td>
+                <td>col4</td>
+                <td>col5</td>
+            </tr>
+            <tr>
+                <td>Utilisateur4</td>
+                <td>2222222222</td>
+                <td>col3</td>
+                <td>col4</td>
+                <td>col5</td>
+            </tr>
+        </tbody>
+    </table>
+
+
+
+
+</template>
