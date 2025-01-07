c52f4f5a08e379f97108dc11613dad116e0dcef7
Floran_Schmid <71209882+FloranSchmid@users.noreply.github.com>
2025-01-07 14:37:28 +0100

    Premier design pages raf

--- src/components/LegalPage.vue
+++ src/components/LegalPage.vue
@@ -1,37 +1,34 @@
 <template>
-    <h2>Legal</h2>
-    <p>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec in mi ac risus feugiat faucibus vitae at neque.
-        Vestibulum id vehicula mauris. Morbi aliquam lorem quis sapien pretium venenatis. Praesent vel arcu velit. Fusce
-        vitae mi tellus. Nunc in felis consequat, aliquam mauris at, rhoncus leo. Integer quis pretium lorem. Fusce nisi
-        sem, interdum sed tellus vitae, feugiat dignissim ante. Nullam id vehicula quam, consectetur convallis nisl.
-        Morbi mollis dignissim tortor, vel rhoncus libero condimentum vel. <br>
-
-        Donec ac mollis ex. Vestibulum commodo purus sapien, in egestas sapien hendrerit sit amet. Fusce blandit porta
-        congue. Duis ultrices, risus vel interdum tincidunt, ligula arcu suscipit lacus, et interdum lectus erat ac mi.
-        Sed dapibus ex neque, ac faucibus risus fringilla nec. Donec dignissim lobortis neque, at lobortis arcu
-        ultricies at. In ligula lectus, fringilla eu justo ut, pretium tincidunt turpis. Curabitur urna urna, porttitor
-        sit amet viverra at, vestibulum in dui. Aliquam erat volutpat. Vivamus blandit enim tempus, maximus enim sit
-        amet, interdum ante. Sed non interdum quam, non efficitur diam. Ut ex erat, pulvinar ac mi id, malesuada
-        vulputate turpis. <br>
-
-        Aenean gravida tempus justo vitae tincidunt. In varius nec metus congue dapibus. Nulla vel convallis massa.
-        Integer tincidunt tincidunt ipsum quis maximus. Sed lacinia eros rhoncus porttitor gravida. Maecenas lacus enim,
-        iaculis sed dolor sit amet, porta sagittis urna. Maecenas sit amet metus vitae quam fringilla semper nec eu
-        enim. Sed efficitur ante vel ligula lobortis, quis maximus libero porttitor. <br>
-
-        Duis hendrerit, velit nec euismod tempor, sem ex ornare lorem, ac viverra lorem sem at velit. Nunc posuere eros
-        neque, in imperdiet leo pretium a. Sed rhoncus ac felis nec sagittis. Donec at sollicitudin elit. Sed tristique
-        ut eros et pellentesque. Morbi sodales condimentum turpis et gravida. Pellentesque libero tellus, ultricies a
-        ultricies et, vehicula eu diam. Nam in ligula metus. Aliquam viverra arcu nulla, at sodales odio suscipit
-        ultrices. Aliquam et magna eu dui sodales tincidunt non eget purus. Phasellus quis iaculis quam. Donec aliquet
-        pellentesque risus. Vestibulum lacinia, orci vel consequat blandit, ex nulla venenatis nibh, sollicitudin
-        sagittis dui ante at tortor. Duis ac leo neque. <br>
-
-        Nam ac purus nec ipsum vestibulum pretium tincidunt ut metus. Ut tincidunt lectus erat. Vestibulum pulvinar
-        mauris eget ligula faucibus elementum. Maecenas eleifend ipsum vel nisi tempor laoreet. Duis pellentesque, nunc
-        eu venenatis lacinia, risus dui feugiat lacus, ut blandit nisl metus ac arcu. Aliquam rutrum quis nunc egestas
-        viverra. Nam consequat tellus ligula, sit amet laoreet metus congue sed. Maecenas viverra elit in augue egestas
-        egestas. <br>
-    </p>
+    <div class="has-background-color has-text-color" style="min-height: 73vh;">
+        <h2>Mentions Légales</h2>
+        <br>
+        <p>
+            Propriété intellectuelle<br>
+            Le contenu (textes, images, graphismes, logos, vidéos, etc.) publié sur le site Locamat est protégé par 
+            le droit de la propriété intellectuelle. Toute reproduction, distribution, modification ou utilisation 
+            partielle ou totale de ces éléments sans autorisation écrite préalable est strictement interdite et 
+            constitue une contrefaçon au sens des articles L.335-2 et suivants du Code de la propriété intellectuelle.
+            <br>
+            <br>
+            Données personnelles<br>
+            Conformément au Règlement Général sur la Protection des Données (RGPD) et à la loi "Informatique et Libertés", 
+            Locamat s'engage à protéger la vie privée des utilisateurs.
+            <br>
+            <br>
+            Cookies<br>
+            Le site Locamat utilise des cookies pour améliorer l'expérience utilisateur. En poursuivant votre navigation, 
+            vous acceptez leur utilisation.
+            <br>
+            <br>
+            Responsabilité<br>
+            Locamat s'efforce de fournir des informations fiables sur son site, mais ne garantit pas l'exactitude, 
+            l'exhaustivité ou la mise à jour de ces informations. La société décline toute responsabilité en cas de 
+            dommages liés à l'utilisation du site.
+            <br>
+            <br>
+            Droit applicable<br>
+            Les présentes mentions légales sont régies par le droit français. Tout litige relatif à leur interprétation 
+            ou à leur exécution sera soumis aux tribunaux compétents.
+        </p>
+    </div>
 </template>
